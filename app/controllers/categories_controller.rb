6eedf16c0d1204ecb0a30e9f7848f05ebb6b361f
Steven Wagner <stevewag86@gmail.com>
2023-06-19 13:17:52 +0200

    Initial commit

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,30 @@
+class CategoriesController < ApplicationController
+
+  def learn
+  end
+
+  def guides
+  end
+
+  def best_practices
+  end
+
+  def red_flag_checklist
+  end
+
+  def faq
+  end
+
+  def fun_stuff
+  end
+
+  def opportunities
+  end
+
+  def blog
+  end
+
+  def links
+  end
+
+end
---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,72 @@
+class TopicsController < ApplicationController
+  
+  def address
+  end
+
+  def blockchain
+  end
+
+  def buy
+  end
+
+  def coinbase_wallet
+  end
+
+  def dao
+  end
+
+  def dapp
+  end
+
+  def defi
+  end
+
+  def dex
+  end
+
+  def ens
+  end
+
+  def exchanges
+  end
+
+  def gas
+  end
+
+  def l2
+  end
+
+  def multisig
+  end
+
+  def nft
+  end
+
+  def poap
+  end
+
+  def qf
+  end
+
+  def smart_contracts
+  end
+
+  def social_recovery
+  end
+
+  def tokens
+  end
+
+  def transacting
+  end
+
+  def wallets
+  end
+
+  def web3
+  end
+
+  def zkproofs
+  end
+
+end
